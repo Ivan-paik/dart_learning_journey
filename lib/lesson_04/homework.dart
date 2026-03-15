c4da753175c28ed4ce463f87e3c847b5e5b58438
Ivan_Paik <uzhgorod2000@yahoo.com>
2026-03-15 14:58:09 +0200

    HW4, task 1.

--- lib/lesson_04/homework.dart
+++ lib/lesson_04/homework.dart
@@ -13,22 +13,38 @@ void main() {
 /// - height (double) — твій зріст у метрах
 /// - name (String) — твоє ім'я
 /// - isStudent (bool) — чи ти студент
-void primitives() {}
+void primitives() {
+  /// create a variables
+  int age = 48;
+  String ageHex = '30';
+  double height = 1.9200;
+  String name = 'iVaN';
+  bool isStudent = true;
+
+  /// print a variables
+  print(
+    'age: $age, height: ${height.round()}, ${height.toStringAsPrecision(2)}, ${height.toStringAsPrecision(9)}, name: ${name.toUpperCase()}, isStudent (${isStudent.runtimeType}): $isStudent',
+  );
+  print('age from hex 0x$ageHex to dec: ${int.parse(ageHex, radix: 16)}');
+}
 
 /// Завдання 2: Колекції
 /// 1. List — 3 твої улюблені кольори
-/// 2. Set — 3 унікальні оцінки, спробуй створити перевір print-ми, що вони не виводяться
+/// 2. Set — 3 унікальні оцінки, спробуй створити перевір print-ми, що вони не
+/// виводяться
 /// 3. Map — 3 предмети та їх оцінки
 void collections() {}
 
 /// Завдання 3: var, final, const
-/// 1. Створи змінну через var — наприклад, місто проживання та зміни її декілька разів (print після кожної зміни)
+/// 1. Створи змінну через var — наприклад, місто проживання та зміни її
+/// декілька разів (print після кожної зміни)
 /// 2. Створи змінну через final — наприклад, країна
 /// 3. Створи змінну через const — наприклад, кількість днів у тижні
 void varFinalConst() {}
 
 /// Завдання 4: Record
-/// Створи іменований та не іменований (позиційний) record з твоїм ім'ям та віком і виведи обидва поля.
+/// Створи іменований та не іменований (позиційний) record з твоїм ім'ям та
+/// віком і виведи обидва поля.
 void record() {}
 
 /// Завдання 5: Nullable (опціонально)
