1741298f3a109f00aa30111b9b50a645cc816321
Ivan_Paik <uzhgorod2000@yahoo.com>
2026-04-12 02:00:56 +0300

    feat(DART-10): finished task 8

---
+++ lib/lesson_10/hw_futures_task8.dart
@@ -0,0 +1,34 @@
+// ignore_for_file:  lines_longer_than_80_chars
+
+// **Task 8: Робота з StreamController**
+// Створіть StreamController<String>.
+// Додайте до цього контролера кілька довільних рядкових значень вручну (наприклад, "Hello", "World", "Dart").
+// Прослухайте цей стрім (через метод listen) і виведіть всі значення у консоль.
+// Закрийте контролер після додавання всіх значень.
+// Використайте callback onDone у методі listen, щоб вивести повідомлення "Стрім завершено" після того, як стрім буде закритий.
+import 'dart:async';
+
+void main() async {
+  print('----------------Початок програми----------------');
+
+  final controller = StreamController<String>();
+
+  controller.add('Hello');
+  controller.add('World');
+  controller.add('Dart');
+
+  controller.close();
+
+  controller.stream.listen(
+    print, //vs code подобається лише цей варіант
+    
+    //(event) => print(event), 
+    
+    // (String event) { 
+    //   print(event);
+    // },
+
+    onDone: () => print('Стрім завершено'),
+  );
+
+}
